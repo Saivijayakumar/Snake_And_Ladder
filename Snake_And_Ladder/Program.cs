9f4881f60e04426f73bda15501b4caccb7c9ad25
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-06-30 19:10:27 +0530

    Generating Random Input

--- Snake_And_Ladder/Program.cs
+++ Snake_And_Ladder/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_And_Ladder
+{
+    class Program
+    {
+        //SnakeAndLadder Program
+        static void Main(string[] args)
+        {
+            //The moment of player tracked by this varable
+            int Position = 0;
+            //creating random object for Random class
+            Random random = new Random();
+            //UC2 Getting a random input
+            //Rolling a die to get a number between 1 and 6
+            int Number = random.Next(1, 7);
+            Console.Read();
+        }
+    }
+}
