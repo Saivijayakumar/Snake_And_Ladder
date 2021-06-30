617d312f9c70b6bb28271328c6dd911b4c8dd397
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-06-30 19:05:36 +0530

    Player at starting position

--- Snake_And_Ladder/Program.cs
+++ Snake_And_Ladder/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_And_Ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SnakeAndLadder Game Starts
+            //Player Is At Starting Position
+            int Position = 0;
+            Console.Read();
+
+        }
+    }
+}
+
